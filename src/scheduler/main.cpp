15fc4f750ced83e21190b02bf1e35ff05e5532f5
chris <cj.magina@gmail.com>
2011-06-23 18:05:06 +0800

    add the cmake file

---
+++ src/scheduler/main.cpp
@@ -0,0 +1,11 @@
+#include "glog/logging.h"
+
+int main(int argc, char ** argv) {
+    google::InitGoogleLogging(argv[0]);
+    google::SetLogDestination(google::INFO, "./log/scheduler_info_");
+    google::SetLogDestination(google::WARNING, "./log/scheduler_warning_");
+    google::SetLogDestination(google::ERROR, "./log/scheduler_error_");
+    google::SetLogDestination(google::FATAL, "./log/scheduler_fatal_");
+    LOG(INFO) << "begin scheduler";
+    return 0;
+}
